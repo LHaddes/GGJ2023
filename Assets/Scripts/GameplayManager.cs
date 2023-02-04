3cf7e2e06ec2ec7f69b65483457393bcd35b3b30
Clément Rivaille <rivaille.clement@orange.fr>
2023-02-04 14:28:34 +0100

    Fuse and unlock fruits

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -1,53 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameplayManager : MonoBehaviour
 {
-    public static GameplayManager Instance;
-    public Fruit fruitToUse;
-    public Tool.ToolType toolToUse;
-
-    public List<Fruit> allFruits = new List<Fruit>();
-    
+  public static GameplayManager Instance;
   public GameInventory inventory;
 
-    void Awake()
-    {
-        Instance = this;
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
+  public UnityEvent onFail = new UnityEvent();
+
+  void Awake()
+  {
+    Instance = this;
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
     inventory.StartGame();
-    }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
 
-    public void Fuse()
+  }
+
+  public void Fuse(Tool.ToolType tool, Fruit fruit)
+  {
+    Fruit newFruit = inventory.GetFusion(tool, fruit);
+    if (newFruit != null)
     {
-        //On vérifie tous les fruits du jeu
-        foreach (Fruit f in allFruits)
-        {
-            //Pour chaque fruit, on vérifie les différentes recettes possibles
-            foreach (Recipe r in f.recipeList)
-            {
-                if (r.fruit == fruitToUse && r.tool == toolToUse)
-                {
-                    Fruit resultFruit = f;  //Si la recette réalisée correspond à une recette exitante, on donne le résultat de la recette au joueur
-                    Debug.Log($"You just crafted {resultFruit.name}");
-                    
-                    return;
-                }
-            }
-        }
-        
-        //Sinon, la recette ne donne rien et on perd le fruit utilisé
-        Debug.Log("Combinaison does not work");
+      inventory.ObtainFruit(newFruit);
     }
+    else
+    {
+      onFail.Invoke();
+    }
+  }
 }
